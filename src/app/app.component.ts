43f595d5c6d7153da09edb4b1eb6d44b3f16caf3
Richard Omoka <richard.omoka@gmail.com>
2021-09-16 19:53:04 +0300

    -Changes to force rebuild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cloudflare-test';
-  let tag = "The Big brown fox jumped over the lazy dog.";
+  tag = 'The Big brown fox jumped over the lazy dog.';
 }
