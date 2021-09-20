c85cb7531a21463f4ea5d2206e31fe14b94ab58d
Richard Omoka <richard@thehillcroftgroup.com>
2021-09-20 17:47:24 +0300

    -macreen test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'cloudflare-test';
   tag = 'The Big brown fox jumped over the lazy dog.';
+  theLists = [
+    { id: '5001', type: 'None' },
+    { id: '5002', type: 'Glazed', mad: true },
+    { id: '5005', type: 'Sugar' },
+    { id: '5007', type: 'Powdered Sugar' },
+    { id: '5006', type: 'Chocolate with Sprinkles', mad: true },
+    { id: '5003', type: 'Chocolate' },
+    { id: '5004', type: 'Maple' },
+  ];
 }
